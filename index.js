e9af62bbed47fa118240fd729f98edde108227c2
Leonardo Souza <leonardosouzasantana9@hotmail.com>
2019-03-17 16:48:55 -0300

    função de detectar comandos no chat

--- index.js
+++ index.js
@@ -4,31 +4,47 @@ const ConfigDiscord = require('./app/controller/configDiscordController');
 
 let configDiscord = new ConfigDiscord();
 
-async function init(){
-   await configDiscord.getById(1).then(successCallback, failureCallback);
-   Client.login(configDiscord.token);
+async function init() {
+    await configDiscord.getById(1).then(successCallback, failureCallback);
+    Client.login(configDiscord.token);
 }
 
 init();
 
 Client.on('ready', () => {
     console.log(`Logado como ${Client.user.tag}`);
+    Client.user.setActivity("Sendo desenvolvido");
 });
 
 Client.on('message', msg => {
-    // console.log(`conteudo`, msg.content);
-    if (msg.content == `ping`)
+    if (msg.content.toLowerCase() == `ping`)
         msg.reply(`pong`);
+    else if (msg.content.startsWith("!"))
+        msg.reply(processarComandos(msg.content));
 
 });
 
 function successCallback(result) {
     configDiscord = result;
-  }
-  
-  function failureCallback(error) {
-    console.log("It failed with " + error);
-  }
+}
+
+function failureCallback(error) {
+    console.log("Deu erro:" + error);
+}
+
+function processarComandos(comando) {
+    let mensagem = '';
+    if (comando.startsWith("!")) {
+        switch (comando.toLowerCase().substring(1)) {
+            case "sad":
+                mensagem = 'Fica triste não';
+                break;
+            default:
+                mensagem = 'Comando não encontrado';
+        }
+    }
+    return mensagem;
+}
 
 
 
